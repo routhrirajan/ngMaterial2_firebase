aed0a89faaf5e5633093ab041db7fd12051fdc4d
Adharsh A.R <Adharsh A.R>
2017-08-13 06:38:46 +0530

    set up .travis.yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCCeAR-d_ecB_u2RrODe7IgOcCCp4TFXYU',
+    authDomain: 'rcsonline-47254.firebaseapp.com',
+    databaseURL: 'https://rcsonline-47254.firebaseio.com',
+    projectId: 'rcsonline-47254',
+    storageBucket: 'rcsonline-47254.appspot.com',
+    messagingSenderId: '492299459995'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCCeAR-d_ecB_u2RrODe7IgOcCCp4TFXYU',
+    authDomain: 'rcsonline-47254.firebaseapp.com',
+    databaseURL: 'https://rcsonline-47254.firebaseio.com',
+    projectId: 'rcsonline-47254',
+    storageBucket: 'rcsonline-47254.appspot.com',
+    messagingSenderId: '492299459995'
+  }
 };
