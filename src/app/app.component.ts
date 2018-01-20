3c3f55dd711a236b13eb19f85d31fe8ebe47b14e
routhrirajan <routhrimanjore@yahoo.com>
2018-01-20 22:35:56 +0530

    Manage Links page modification and image gallery addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component, ViewChild, Directive } from '@angular/core';
+import {
+  Router,
+  // import as RouterEvent to avoid confusion with the DOM Event
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router'
+import {Component, Directive, NgZone, Renderer, ElementRef, ViewChild} from '@angular/core'
 import { AnimationsService } from './shared/animations/index';
 
 @Component({
@@ -9,32 +18,82 @@ import { AnimationsService } from './shared/animations/index';
 })
 export class AppComponent {
   public activateAnimation = false;
+  isDarkTheme = false;
 
+  // Instead of holding a boolean value for whether the spinner
+  // should show or not, we store a reference to the spinner element,
+  // see template snippet below this script
+  @ViewChild('spinnerElement')
+  spinnerElement: ElementRef
   /**
    * Constructor of the class
    *
    * @param {AnimationsService} animationService
    */
-  public constructor(
+  public constructor(private router: Router,
+    private ngZone: NgZone,
+    private renderer: Renderer,
     private animationService: AnimationsService
-  ) 
-  {
+  ) {
     this.animationService.activateAnimation$.subscribe(
       (value) => this.activateAnimation = value
     );
+    router.events.subscribe((event: RouterEvent) => {
+      this._navigationInterceptor(event)
+    })
   }
-  isDarkTheme: boolean = false;  
+  // Shows and hides the loading spinner during RouterEvent changes
+  private _navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      // We wanna run this function outside of Angular's zone to
+      // bypass change detection
+      this.ngZone.runOutsideAngular(() => {
+        // For simplicity we are going to turn opacity on / off
+        // you could add/remove a class for more advanced styling
+        // and enter/leave animation of the spinner
+        this.renderer.setElementStyle(
+          this.spinnerElement.nativeElement,
+          'display',
+          'flex'
+        )
+      })
+    }
+    if (event instanceof NavigationEnd) {
+      this._hideSpinner()
+    }
+    // Set loading state to false in both of the below events to
+    // hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this._hideSpinner()
+    }
+    if (event instanceof NavigationError) {
+      this._hideSpinner()
+    }
+  }
+  private _hideSpinner(): void {
+    // We wanna run this function outside of Angular's zone to
+    // bypass change detection,
+    this.ngZone.runOutsideAngular(() => {
+      // For simplicity we are going to turn opacity on / off
+      // you could add/remove a class for more advanced styling
+      // and enter/leave animation of the spinner
+      this.renderer.setElementStyle(
+        this.spinnerElement.nativeElement,
+        'display',
+        'none'
+      )
+    })
+  }
   onThemeChange(status: boolean) {
     this.isDarkTheme = status;
   }
   toggleSidebar(status: boolean) {
     if (status) {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById('mySidenav').style.width = '250px';
+      document.getElementById('main').style.marginLeft = '250px';
+    } else {
+      document.getElementById('mySidenav').style.width = '0';
+      document.getElementById('main').style.marginLeft = '0';
     }
-    else {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-    }
-  }  
+  }
 }
--- src/app/links/links-detail/links-detail.component.ts
+++ src/app/links/links-detail/links-detail.component.ts
@@ -1,56 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MdSnackBar } from '@angular/material'
+import { MdSnackBar, MdDialog, MdDialogRef } from '@angular/material'
 
 import { ILink } from '../interfaces';
 import { ICategories, CategoriesService } from 'app/categories';
 import { LinksService } from '../services';
-import { Params, ActivatedRoute } from '@angular/router';
-import { FirebaseObjectObservable } from 'angularfire2/database';
+import { ActivatedRoute } from '@angular/router';
+import { ImagesListComponent } from 'app/links/images-list/images-list.component';
 
 @Component({
   selector: 'app-links-detail',
   templateUrl: './links-detail.component.html',
   styleUrls: ['./links-detail.component.css']
 })
-export class LinksDetailComponent implements OnInit {
-linksForm: FormGroup;
-link: FirebaseObjectObservable<ILink>;
-selectedLink: any;
-categories: any;
+export class LinksDetailComponent {
+  linksForm: FormGroup;
+  link: any;
+  categories: any;
   constructor(
     private fb: FormBuilder,
     private _categoriesService: CategoriesService,
     private _linksService: LinksService,
     private _snackBar: MdSnackBar,
+    private _dialog: MdDialog,
     private route: ActivatedRoute
-  ) { }
+  ) {
+    this.createForm();
+  }
 
-  ngOnInit() {
-    // this.route.params.switchMap((params: Params) => this._linksService.getLink(params['id']))
-    // .subscribe(link => this.link = link);
+  createForm() {
     this.route.params.subscribe(params => this.link = this._linksService.getLink(params['id']));
-    this.categories = this._categoriesService.getCategoriesList();
+
     this.link.subscribe(link => {
-    this.linksForm = this.fb.group({
-      key: [link === void 0 ? '' : link.$key],
-      name: [link === void 0 ? '' : link.name, [Validators.required, Validators.minLength(3)]],
-      image: [link === void 0 ? '' : link.image],
-      url: [link === void 0 ? '' : link.url],
-      status: [link === void 0 ? true : link.status],
-      category: [link === void 0 ? '' : link.category],
-      //categories: [this.categories]
+      this.linksForm = this.fb.group({
+        key: [link === void 0 ? '' : link.$key],
+        name: [link === void 0 ? '' : link.name, [Validators.required, Validators.minLength(3)]],
+        image: [link === void 0 ? '' : link.image],
+        url: [link === void 0 ? '' : link.url],
+        status: [link === void 0 ? true : link.status],
+        category: [link === void 0 ? '' : link.category],
+      })
     })
-    })
+    this.categories = this._categoriesService.getCategoriesList();
+  }
+  openChooseGallery() {
+    let _dialogRef: MdDialogRef<ImagesListComponent>;
+    _dialogRef = this._dialog.open(ImagesListComponent);
   }
   onSubmit() {
-    console.log(this.linksForm.get('key').value);
     if (this.linksForm.get('key').value) {
       this._linksService.updateLink(this.linksForm.get('key').value, this.linksForm.value)
       this._snackBar.open('Links Updated', 'OK');
     } else {
-    this._linksService.createLink(this.linksForm.value);
-    this._snackBar.open('Links Created', 'OK');
+      this._linksService.createLink(this.linksForm.value);
+      this._snackBar.open('Links Created', 'OK');
     }
   }
 }
